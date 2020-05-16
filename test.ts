49cd30d7b11f50becf3ecc669922304a6b537bc1
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2020-05-16 16:45:46 +0900

    Updates.

--- test.ts
+++ test.ts
@@ -1,1 +1,7 @@
 // tests go here; this will not be compiled when this package is used as a library
+basic.forever(function () {
+    for (let d = 0; d <= 180; d += 10) {
+        softservo.softServo(DigitalPin.P1, d)
+        basic.pause(1000)
+    }
+})
